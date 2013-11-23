491f28fc3435a0c843a8f7c175df4aa292324a4a
Robert Taraya <robert.taraya@gmail.com>
2013-11-22 18:02:28 -0600

    add all 4 game rules to game

--- game_of_life.js
+++ game_of_life.js
@@ -2,6 +2,7 @@ window.onload = function(){
   console.log('window loaded!');
 
   var game = new Game(20,25)
+  game.cellLiveOrDie(20,25)
 };
 
 
@@ -11,4 +12,32 @@ var Game = function(rows,cols){
   this.board = new Board(rows,cols)
 }
 
-game.prototype
+Game.prototype.cellLiveOrDie = function(rows, cols){
+  for (var x=1; x<=rows; x++){
+    for (var y=1; y<=cols; y++){
+      var liveCellCount = Cell.prototype.findLiveNeighbors(x,y)
+      var cellId = ""
+      cellId = cellId + "row" + x
+      cellId = cellId + "col" + y
+      cellInQuestion = document.getElementById(cellId)
+      if (cellInQuestion.className==='active'){
+        cell is alive
+        if (liveCellCount < 2){
+          cellInQuestion.className='inactive'
+        } else if (liveCellCount === 2 || liveCellCount === 3){
+          cellInQuestion.className='active'
+        } else if (liveCellCount > 3){
+          cellInQuestion.className='inactive'
+        }
+      } else {
+        if (liveCellCount === 3){
+          cellInQuestion.className='active'
+        }
+      }
+    }
+  }
+}
+
+Game.prototype.runIT = function(){
+
+}
