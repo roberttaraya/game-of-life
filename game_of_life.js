4c16b91664f68001c4511fd1b345c6216bf70e59
Robert Taraya <robert.taraya@gmail.com>
2013-11-23 00:50:44 -0600

    it works!

--- game_of_life.js
+++ game_of_life.js
@@ -1,27 +1,26 @@
 window.onload = function(){
-  console.log('window loaded!');
-
   var game = new Game(20,25)
-  game.cellLiveOrDie(20,25)
+  game.runIT(20,55)
 };
 
 
-
 var Game = function(rows,cols){
-  console.log("Game: a new game has started");
+  this.cell = new Cell(rows,cols)
   this.board = new Board(rows,cols)
+  // debugger
 }
 
 Game.prototype.cellLiveOrDie = function(rows, cols){
-  for (var x=1; x<=rows; x++){
-    for (var y=1; y<=cols; y++){
-      var liveCellCount = Cell.prototype.findLiveNeighbors(x,y)
+  for (var x=1; x<=20; x++){
+    for (var y=1; y<=25; y++){
+      // debugger
+      var liveCellCount = this.cell.findLiveNeighbors(x,y)
       var cellId = ""
       cellId = cellId + "row" + x
       cellId = cellId + "col" + y
       cellInQuestion = document.getElementById(cellId)
+      // debugger
       if (cellInQuestion.className==='active'){
-        cell is alive
         if (liveCellCount < 2){
           cellInQuestion.className='inactive'
         } else if (liveCellCount === 2 || liveCellCount === 3){
@@ -35,9 +34,24 @@ Game.prototype.cellLiveOrDie = function(rows, cols){
         }
       }
     }
+      // debugger
   }
 }
 
-Game.prototype.runIT = function(){
-
+Game.prototype.runIT = function(rows, cols){
+  var oldTable = ""
+  var newTable = document.getElementById("game-table")
+  var count = 0
+  // while(oldTable !== newTable){
+  // for (var i = 1; i <= 1000; i++){
+  var self = this
+  setInterval(function(){
+    self.cellLiveOrDie(rows, cols)
+    count += 1
+    console.log("count:", count)
+  }, 500)
+    // oldTable = newTable
+    // newTable = document.getElementById("game-table")
+    // debugger
+  // }
 }
